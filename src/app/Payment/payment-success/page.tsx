f8e51aed95159cf53bcf7e00fca44a8c60118fce
talalhassankhan18 <vividclicks2.0@gmail.com>
2025-05-20 13:33:31 +0500

    Final update

--- src/app/Payment/payment-success/page.tsx
+++ src/app/Payment/payment-success/page.tsx
@@ -4,19 +4,20 @@ interface PageProps {
   };
 }
 
-export default function PaymentSuccess({ searchParams }: PageProps) {
-  const amount = searchParams.amount ?? "0.00"; // fallback if amount is undefined
+const PaymentSuccess = async ({ searchParams }: PageProps) => {
+  const amount = searchParams?.amount ?? "0.00";
 
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
       <div className="mb-10">
         <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
         <h2 className="text-2xl">You successfully sent</h2>
-
         <div className="bg-white p-2 rounded-md text-purple-500 mt-5 text-4xl font-bold">
           ${amount}
         </div>
       </div>
     </main>
   );
-}
+};
+
+export default PaymentSuccess;
